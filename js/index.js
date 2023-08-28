afd1d09fc0a6663d8f0f7035b06b8ee6735ac83b
BlackJackMC <duytu85343@gmail.com>
2023-08-28 19:41:57 +0700

    Update home styling

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const setHeaderPadding = (targetQuery) => {
     const target = document.querySelector(`${targetQuery}`);
     const header = document.querySelector('header');
     const headerHeight = parseInt(window.getComputedStyle(header).height.match("[0-9]*")[0]);
-    target.style.paddingBlock = `${8 * 16 + headerHeight}px`;
+    target.style.paddingBlockStart = `${8 * 16 + headerHeight}px`;
 }
 
 const boxList = document.querySelectorAll('.box');
@@ -10,9 +10,16 @@ const boxList = document.querySelectorAll('.box');
 boxList.forEach((e) => {
     const content = e.querySelector(".box-content");
     const button = e.querySelector("a[type=button][aria-controls=box-content]");
+    const buttonExpandedText = button.querySelector("span.box-button-expand");
+    const buttonCollapsedText = button.querySelector("span.box-button-collapse");
+    const flip = () => {
+        [buttonExpandedText.style.display, buttonCollapsedText.style.display]
+      = [buttonCollapsedText.style.display, buttonExpandedText.style.display];
+    };
     
     if (content.scrollHeight > content.clientHeight) {
-        button.style.display = 'inline';
+        button.style.display = buttonExpandedText.style.display = 'inline';
+        buttonCollapsedText.style.display = 'none';
     } else {
         button.style.display = 'none';
     }
@@ -20,10 +27,10 @@ boxList.forEach((e) => {
     button.addEventListener('click', (event) => {
         if (content.ariaExpanded === "false") {
             content.ariaExpanded = "true"
-            button.innerText = "Read less";
+            flip();
         } else {
             content.ariaExpanded = "false"
-            button.innerText = "Read more";
+            flip();
         }
     })
 })
